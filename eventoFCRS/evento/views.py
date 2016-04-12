144a982e7cb5b1ddbc7d7ffc8e8326441af07746
mardou01 <marcos.douglas.10@hotmail.com>
2016-04-12 12:54:10 -0300

    passando parametro

--- eventoFCRS/evento/views.py
+++ eventoFCRS/evento/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello World!")
+    return render(request, 'evento/home.html',{
+    'nome': "Marcos Douglas",
+    'idade':"18 anos",
+    'altura':"1,68 metros",
+    })
